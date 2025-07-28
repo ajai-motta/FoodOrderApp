f7457253d4ee5b83e197f95cda184609507f2887
Ajai Joseph <ajayjoseo8@gmail.com>
2025-07-28 20:24:22 +0530

    5

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -15,16 +15,35 @@ export default function CheckOut({}){
     function handleCloseCheckout(){
         userContext.hideCheckout();
     }
+    function handleSubmit(event){
+        event.preventDefault()
+        const eventData=new FormData(event.target);
+        const consumerData=Object.fromEntries(eventData.entries());
+        fetch('http://localhost:3000/orders',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body: JSON.stringify({
+               order: {
+
+                    items: cartCtx.items,
+                    customer: consumerData
+                }
+            })
+        })
+
+    }
     return <Modal open={userContext.progress==='checkout'} onClose={handleCloseCheckout}>
-        <form >
+        <form onSubmit={handleSubmit}>
             <h2>Checkout</h2>
             <p>Total Amount: {formmater.format(cartTotal)}</p>
-            <Input label="Full name" type='text' id="Full-name"/>
+            <Input label="Full name" type='text' id="name" />
             <Input label="E-mail Address" type='email' id="email"/>
-            <Input label="Street" type='text' id="Street"/>
+            <Input label="Street" type='text' id="street"/>
             <div className="control-row">
-                <Input label="Postal Code" type='text' id="Postal-code"/>
-                <Input label="City" type='text' id="City"/>
+                <Input label="Postal Code" type='text' id="postal-code"/>
+                <Input label="City" type='text' id="city"/>
             </div>
             <p className="model-actions">
                 <Button type="button" onText onClick={handleCloseCheckout} isCart={false}>Close</Button>
